6748612bc7d5df18fa94c2f398bef0d69be5f7de
Gabriel Adolfo Díaz Sosa <gabrieladolfod@gmail.com>
2023-05-26 22:03:17 -0600

    Agregando comentarios

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-var btnEncriptar = document.getElementById("btn-encriptar");
-var btnDesencriptar = document.getElementById("btn-desencriptar");
-var btnCopiar = document.getElementById("btn-copiar")
-var input = document.getElementById("text-area");
-var parrafo = document.getElementById("msg");
-var validacionMinusculas = "^[ a-zñ¿?!¡]+$";
-var descripcion = document.getElementById("descripcion");
+var btnEncriptar = document.getElementById("btn-encriptar"); //Boton encriptar
+var btnDesencriptar = document.getElementById("btn-desencriptar"); //Boton desencriptar
+var btnCopiar = document.getElementById("btn-copiar") //Boton para copiar el texto encriptado o desenciptado
+var input = document.getElementById("text-area"); //textarea que sera nuestro input de datos
+var parrafo = document.getElementById("msg"); //<p> que mostrara nuestro resultado de encriptar o desencriptar
+var validacionMinusculas = "^[ a-zñ¿?!¡]+$";  //Expresion regular para solo permitir minusculas y signos de admiracion e interrogacion 
+var descripcion = document.getElementById("descripcion"); // <div> que muestra los mensaje debajo de la imagen
 
+//Toma del texto y validamos que solo hay letras minusculas
 function tomarDatos() {
     if (input.value.match(validacionMinusculas) != null) {
-
         var mensaje = input.value;
         return mensaje;
     }
-
+//Mostramos mensaje en caso de que se ingresara algun caracter invalido.
     if (input.value.match(validacionMinusculas) == null) {
         console.log("Letras con Espacio Invalido");
         parrafo.innerHTML = "Error, solo se permiten letras minusculas.";
@@ -20,7 +20,7 @@ function tomarDatos() {
         return null;
     }
 }
-
+//Encriptamos el mensaje
 function encriptar() {
     var stringEncriptada = tomarDatos();
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
@@ -31,13 +31,9 @@ function encriptar() {
             stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
         }
     }
-    parrafo.style.backgroundImage = "none";
-    descripcion.style.display = "none";
-    btnCopiar.style.display = "block";
-    parrafo.innerHTML = "" + stringEncriptada;
-    input.value = "";
+    mostrar(stringEncriptada); //Llamamos a la funcion mostrar para devolver el resultado
 }
-
+//Desencriptamos el mensaje
 function desencriptar() {
     var stringDesencriptada = tomarDatos();
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
@@ -48,18 +44,24 @@ function desencriptar() {
             stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
         }
     }
-    parrafo.style.backgroundImage = "none";
-    descripcion.style.display = "none";
-    btnCopiar.style.display = "block";
-    parrafo.innerHTML = "" + stringDesencriptada;
-    input.value = "";
+    mostrar(stringDesencriptada); //Llamamos a la funcion mostrar para devolver el resultado
 }
 
+//Copiar el mensaje encriptado o desenciptado
 function copiar() {
     let copyText = document.getElementById("msg");
     navigator.clipboard.writeText(copyText.textContent);
 }
 
+//Devolvemos el mensaje encriptado o desencriptado
+function mostrar(msgRespuesta){
+    parrafo.style.backgroundImage = "none"; //Ocultamos imagen del muñeco
+    descripcion.style.display = "none"; //Ocultamos los mensaje debajo del muñeco
+    btnCopiar.style.display = "block"; //Hacemos visible el boton de copiar para su uso
+    parrafo.innerHTML = "" + msgRespuesta; //Imprimimos respuesta del mensaje
+    input.value = ""; //Limpiamos nuestro textarea 
+}
+//Llamando a los eventos
 btnEncriptar.onclick = encriptar;
 btnDesencriptar.onclick = desencriptar;
 btnCopiar.onclick = copiar;
